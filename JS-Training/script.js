7a4a46191601c68f4e51992e94ed0175a6d6de98
Andy <andymakeham@yahoo.com>
2017-03-22 06:58:42 +0000

    js training update

---
+++ JS-Training/script.js
@@ -0,0 +1,39 @@
+/*
+var name ='john'
+console.log(name);
+
+var lastName ="Smith";
+console.log(lastName);
+
+var age = 26;
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
+*/
+
+// Lecture: variables 2
+var name = 'John';
+var age = 26;
+
+console.log (name + age);
+
+console.log(age + age);
+
+var job, isMarried;
+console.log(job); 
+
+job = "teacher";
+isMarried = true;
+
+console.log(name + ' is ' + age + ' and a ' + 'teacher' + ' .Is he married? ' + isMarried + '.');
+
+age = 'thirty six';
+job = 'driver';
+
+console.log(name + ' is ' + age + ' and a ' + 'teacher' + ' .Is he married? ' + isMarried + '.');
+
+/*var newMessage = prompt('what is your name?');
+console.log(newMessage);*/
+
+alert (name + ' is ' + age + ' and a ' + 'teacher' + ' .Is he married? ' + isMarried + '.');
