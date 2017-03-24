5d871db3874b6296c1c5b1f2e8d295601556a0ad
Andy <andymakeham@yahoo.com>
2017-03-24 06:57:20 +0000

    changes6:57

---
+++ JS-Training/script-if-else.js
@@ -0,0 +1,10 @@
+var name = "john";
+var age = 52;
+var isMarried = "no";
+
+if (isMarried === 'yes') {
+console.log(name + ' is married');
+} else {
+console.log(name + ' will hopefully marry soon');
+
+}
